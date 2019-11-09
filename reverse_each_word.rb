4f45ad131d821d989d39be50a2195c1f07395779
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-09 15:57:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   reversed_sentence = []
-  sentence.split(" ")
-  sentence.each do |word|
+  array_sentece = sentence.split(" ")
+  array_sentece.each do |word|
     reversed_sentence << word.reverse
   end
   reversed_sentence
