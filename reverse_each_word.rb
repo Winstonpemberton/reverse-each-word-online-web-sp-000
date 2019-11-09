7b9367a31c3bc51d8bd801087b7c1067c3afd7f0
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-09 16:06:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence)
   reversed_sentence = []
   array_sentece = sentence.split(" ")
   array_sentece.map do |word|
-    #reversed_sentence << word.reverse
-    word.reverse
+    reversed_sentence << word.reverse
+    #word.reverse
   end
-  #reversed_sentence.join(" ")
-  array_sentece.join(" ")
+  reversed_sentence.join(" ")
+  #array_sentece.join(" ")
 end
